aa892ca52aeb6da641015a4875d6571a818c9b32
Ömer Hamid Kamışlı <hamid.kamisli@magis.com.tr>
2022-02-02 12:36:26 +0300

    Initial Commit

---
+++ scoutium-case/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scoutium-case
+//
+//  Created by Ömer Hamid Kamışlı on 2.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
